0559f3a552937028dc4347a2e5101753fe62af54
Roman <91662434+F1nery@users.noreply.github.com>
2023-11-19 21:42:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var greetingElement = document.getElementById("greeting");
+var greetingText = "Вітаємо вас з наступаючим новим роком!";
+var index = 0;
+
+function typeWriter() {
+    if (index < greetingText.length) {
+        greetingElement.textContent += greetingText.charAt(index);
+        index++;
+        setTimeout(typeWriter, 100); // Час затримки між виведенням символів (в мілісекундах)
+    }
+}
+
+// Викликаємо функцію для початку поступового друкування тексту
+typeWriter();
